164a21ab274efccdd2b85545010a227fd2576303
seothemes <seothemeswp@gmail.com>
2020-05-02 09:02:04 +0800

    Add demo list rest api endpoint

--- lib/init.php
+++ lib/init.php
@@ -30,3 +30,30 @@ function mai_demo_exporter( $template ) {
 
 	return $template;
 }
+
+add_action( 'rest_api_init', 'mai_demo_exporter_list_demos' );
+
+function mai_demo_exporter_list_demos() {
+	register_rest_route( 'mai-demo-exporter/v2', '/sites/', [
+		'methods'  => 'GET',
+		'callback' => 'mai_demo_exporter_list_demos_callback',
+	] );
+}
+
+function mai_demo_exporter_list_demos_callback( $request ) {
+	$response = new WP_Error( 'not_multisite', __( 'Not a multisite install' ) );
+	$sites    = get_sites();
+
+	if ( $sites && isset( $request['theme'] ) ) {
+		$response = [];
+		$theme    = sanitize_text_field( $request['theme'] );
+
+		foreach ( $sites as $site ) {
+			if ( mai_has_string( $theme, $site->path ) ) {
+				$response[] = str_replace( [ $theme, '-', '\\', '/' ], '', $site->path );
+			}
+		}
+	}
+
+	return new WP_REST_Response( $response );
+}
