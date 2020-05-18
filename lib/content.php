5a86ecdf33f583a46c68305a91bbb319fc130b3f
seothemes <seothemeswp@gmail.com>
2020-05-18 18:18:54 +0800

    Only include attachments that are used as featured images

--- lib/content.php
+++ lib/content.php
@@ -45,6 +45,50 @@ function mai_demo_exporter_content() {
 	}
 
 	/**
+	 * Get all featured image IDs.
+	 *
+	 * @var DOMElement $meta_key
+	 */
+	$featured_image_ids = [];
+
+	foreach ( $xpath->query( '//wp:meta_key' ) as $meta_key ) {
+		$postmeta = $meta_key->parentNode;
+
+		foreach ( $postmeta->childNodes as $child_node ) {
+			if ( 'wp:meta_value' === $child_node->nodeName ) {
+				$featured_image_ids[] = $child_node->textContent;
+			}
+		}
+	}
+
+	/**
+	 * Get all attachment IDs.
+	 *
+	 * @var DOMElement $post_type
+	 */
+	foreach ( $xpath->query( '//wp:post_type' ) as $post_type ) {
+		$c_data = $post_type->textContent;
+
+		if ( 'attachment' === $c_data ) {
+			$post    = $post_type->parentNode;
+			$post_id = '';
+
+			/**
+			 * @var $child_node DOMElement
+			 */
+			foreach ( $post->childNodes as $child_node ) {
+				if ( 'wp:post_id' === $child_node->nodeName ) {
+					$post_id = $child_node->textContent;
+				}
+			}
+
+			if ( ! in_array( $post_id, $featured_image_ids, true ) ) {
+				$post->parentNode->removeChild( $post );
+			}
+		}
+	}
+
+	/**
 	 * Limit posts to 9.
 	 *
 	 * @var DOMElement $post_type
@@ -52,10 +96,11 @@ function mai_demo_exporter_content() {
 	$counter = 1;
 
 	foreach ( $xpath->query( '//wp:post_type' ) as $post_type ) {
-		$item  = $post_type->parentNode;
-		$inner = $post_type->textContent;
+		$c_data = $post_type->textContent;
 
-		if ( 'post' === $inner ) {
+		if ( 'post' === $c_data ) {
+			$item = $post_type->parentNode;
+
 			if ( 6 < $counter ) {
 				$item->parentNode->removeChild( $item );
 			}
